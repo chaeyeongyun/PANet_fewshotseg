308ce4d685120c2e40fa3d65d9b04e0260dee2f3
chaeyeongyun <ui88g216@gmail.com>
2023-01-16 18:21:26 +0900

    train_frame_n_dataset

--- PANet.py
+++ PANet.py
@@ -6,29 +6,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from data import FewshotData
-from warmup_scheduler import GradualWarmupScheduler
-
 
 class PANet():
     def __init__(self, opt):
-        self.device_setting(opt.device)
-        self.batchsize = opt.batchsize
+        if opt.mode == 'train':
+            self.device_setting(opt.device)
+            self.batchsize = opt.batchsize
+            self.scaler = opt.scaler
+            self.align_loss_scaler = opt.align_loss_scaler
+            self.feature_extractor = models.MVGG16(init_weights=True)
+            #TODO: load data
+            self.dataset = make_data('...')
+            self.optimizer = torch.optim.Adam(self.feature_extractor.parameters(), lr=opt.init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0001, amsgrad=False)
+            self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, len(self.dataset), eta_min=1e-7, verbose=True)
+            self.ce_loss = nn.CrossEntropyLoss()
+            self.ckpoint_path = os.path.join(self.save_path, 'ckpoints')
         
-        self.feature_extractor = models.MVGG16(init_weights=True)
-        self.optimizer = torch.optim.Adam(self.feature_extractor.parameters(), lr=opt.init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0001, amsgrad=False)
-        # self.lr_scheduler = 
-        self.ce_loss = nn.CrossEntropyLoss(ignore_index=opt.ignore_idx)
-        #TODO: align loss
-        # self.align_loss = 
     def train(self,):
-        #TODO: load data
-        dataset = make_data('...')
         trainloader = DataLoader(dataset, self.batchsize)
+        best_loss = 100
         for iter, batch in enumerate(trainloader):
             support_imgs = [[shot.to(self.device) for shot in way] for way in batch['support imgs']]
             support_fg_masks = [[shot.to(self.device) for shot in way]for way in batch['support_fg_masks']]
             support_bg_masks = [[shot.to(self.device) for shot in way] for way in batch['support_bg_masks']]
             query_imgs =  [query_img.to(self.device) for query_img in batch['query_imgs']]
+            #TODO: query labels shape?
             query_labels = [query_label.to(self.device) for query_label in batch['query_labels']]
             
             self.optimizer.zero_grad()
@@ -48,6 +50,7 @@ class PANet():
             # [(shotxB, 3, H, W) x n_ways]여기에 쿼리이미지들까지 (n_queryxB, 3, H, W)해서 concat하면
             # ((n_waysxshotxB + n_queryxB, 3, H, W)의 input이 만들어짐. 이것을 input으로 넣음
             input = torch.cat([torch.cat(way, dim=0) for way in support_imgs]+[torch.cat(query_imgs, dim=0)], dim=0)
+            inp_size = tuple(input.shape[-2:]) # input image size
             features = self.feature_extractor(input)
             feat_h, feat_w = features.shape[-2:]
             support_features = features[:n_ways*n_shots*self.batchsize].view(n_ways, n_shots, self.batchsize, -1, feat_h, feat_w) #(n_ways, n_shots, B, C, feath, featw)
@@ -67,26 +70,40 @@ class PANet():
                                for way in range(n_ways)]
                 fg_prototypes, bg_prototype = self.get_prototype(map_fg_fts, map_bg_fts)
                 prototypes = [bg_prototype] + fg_prototypes
-            #TODO: 여기부터 시작해야쥬 프로토타입까지 계산했고 이제 distance 연산하는 부분!
-            # distance = 
-            # pred = 
-            # outputs += [F.interpolate(pred, size=imgsize, mode='bilinear')]
-            ### compute the distance ###
+                # distance between features (query) and prototypes
+                distance = [F.cosine_similarity(query_features[:, epi], prototype, dim=1)*self.scaler for prototype in prototypes]
+                # query_features[:, epi] 는 (N_query, C, H, W), 각 prototype은 (1, C) 일텐데...
+                # (N_query, H, W)의 way+1 (클래스 수 + 배경 클래스) 개 리스트가 될 것이고 이것을 stack해준다
+                pred = torch.stack(distance, dim=1) # (n_query, (1+way), H', W')
+                outputs.append(F.interpolate(pred, size=inp_size, mode='bilinear')) 
+                ## align loss 
+                align_loss += self.align_loss(query_features[:, epi], pred, support_features[:, :, epi],
+                                              fg_mask[:, :, epi], bg_mask[:, :, epi])
+            query_pred = torch.stack(outputs, dim=1) # query 예측 결과들 (n_query, num_epi, 1+n_ways, H, W)
+            query_pred = query_pred.view(-1, *query_pred.shape[2:]) # (n_query*num_epi, 1+n_ways, H, W)
+            align_loss /= self.batchsize
+            ce_loss = self.ce_loss(query_pred, query_labels)
+            total_loss = ce_loss + align_loss * self.align_loss_scaler
+            if best_loss >= total_loss.item():
+                best_loss = total_loss.item()
+                torch.save(self.feature_extractor.state_dict(), os.path.join(self.ckpoint_path, f'best_loss_{iter}iter.pth'))    
+            total_loss.backward()
+            self.optimizer.step()
+            self.lr_scheduler.step()
             
-            
-            # align_loss += self.align_loss(query_features[:, epi], pred, support_features[:, :, epi],
-            #                               fg_mask[:, :, epi], bg_mask[:, :, epi])
-            
+            torch.save(self.feature_extractor.state_dict(), os.path.join(self.ckpoint_path, f'{iter}iter.pth'))
+        torch.save(self.feature_extractor.state_dict(), os.path.join(self.ckpoint_path, f'model_last.pth'))
             
     def map(self, features, mask):
         ''' 
         masked average pooling
-        features shape: (1, C, H, W)
+        features shape: (1, C, H', W')
         mask shape: (1, H, W) -> unsqueeze is needed to multiply to features
         '''
         masked_features = F.interpolate(features, size=mask.shape[-2:], mode='bilinear') * \
             torch.unsqueeze(mask, dim=0)
         masked_features = torch.sum(masked_features, dim=(2,3)) / (mask[None, ...].sum(dim=(2, 3)) + 1e-5) # (1, C)
+        return masked_features
     
     def get_prototype(self, map_fg_fts, map_bg_fts):
         """average the features that is applied map to extract prototype
@@ -100,18 +117,65 @@ class PANet():
         bg_prototype = sum([sum(way) / n_shots for way in map_bg_fts]) / n_ways # mean of mean of shots in ways -> 그냥 background 하나의 prototype
         return fg_prototypes, bg_prototype
     
+    def align_loss(self, q_ft, pred, sup_ft, fg_mask, bg_mask):
+        """
+        Compute the loss for the prototype alignment branch
+        Args:
+            q_ft: embedding features for query images of one episode
+                shape: N x C x H' x W'
+            pred: predicted segmentation score for one episode
+                shape: N x (1 + Wa) x H x W
+            sup_ft: embedding features for support images of one episode
+                shape: Wa x Sh x C x H' x W'
+            fg_mask: foreground masks for support images of one episode
+                shape: way x shot x H x W
+            bg_mask: background masks for support images of one episode
+                shape: way x shot x H x W
+        """
+        n_ways, n_shots = len(fg_mask), len(fg_mask[0])
+        inp_size = tuple(fg_mask.shape[-2:])
+        # get prototype from query
+        pred_result = pred.argmax(dim=1, keepdim=True) # (N_query, 1, H', W')
+        binary_masks = [pred_result == i for i in range(1+n_ways)] # a list of (N_query, 1, H', W') tensors, each tensor have 0 or 1 values.
+        # pred되지 않은 classe들 (way들) 은 skip 합니다
+        skip_ways = [i  for i in range(1, n_ways+1) if binary_masks[i].sum() == 0] # 예측결과는 bg + n_ways이므로
+        query_masks = torch.stack(binary_masks, dim=1).float() #(N_query, 1+n_ways, 1, H', W')
+        query_prototypes = (q_ft.unsqueeze(1) * query_masks) # (N, 1, C, H', W')와 (N_query, 1+n_ways, 1, H', W') 의 곱이 broad castint의해 (N, 1+n_ways, C, H', W') 의 결과를 낸다.
+                                            # 각 쿼리별 각 클래스에 대한 masked feature들이다.
+        query_prototypes = torch.sum(query_prototypes, dim=(0, -2, -1)) # (1+n_ways, C)
+        query_prototypes = query_prototypes / query_masks.sum(dim=(0, -2, -1) + 1e-5) # average
+        # compute loss
+        loss = 0
+        for way in range(n_ways):
+            if way in skip_ways: continue
+            bg_prototype, fg_prototype = query_prototypes[[0]], query_prototypes[[way+1]] # (1, C)
+            for shot in range(n_shots):
+                features = sup_ft[way, [shot]] #(1, C, H', W')
+                supft_qproto_dist = [F.cosine_similarity(features, bg_prototype, dim=1)*self.scaler,
+                                     F.cosine_similarity(features, fg_prototype, dim=1)*self.scaler]
+                sup_pred = torch.stack(supft_qproto_dist, dim=1) # (1, 2, H', W')
+                sup_pred = F.interpolate(sup_pred, size=inp_size, mode='bilinear') # (1, 2, H, W)
+                # binary label (GT). fg:1, bg:0, (H, W)
+                sup_label = torch.full_like(fg_mask[way, shot], -1).long().to(features.device)
+                sup_label[fg_mask[way, shot]==1] = 1
+                sup_label[bg_mask[way, shot]==1] = 0
+                loss += F.cross_entropy(sup_pred, sup_label.unsqueeze(0), ignore_index=-1) / (n_shots*n_ways)
+        return loss
+    
     def device_setting(self, device):
         if device != '-1' and torch.cuda.is_available():
             self.device = torch.device('cuda'+device)
         elif device == '-1':
             self.device = torch.device('cpu')
         else: raise Exception('device number is not available or torch.cuda.is_available is False')
-    
-    
+
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, deault='train', help='mode')
+    parser.add_argument('--save_path', type=str, deault='./train', help='path for saving')
     parser.add_argument('--device', type=str, deault='0', help='gpu number to use. -1 is cpu')
     parser.add_argument('--init_lr', type=float, deault=1e-4, help='initial learning rate')
-    parser.add_argument('--ignore_idx', type=float, deault=1e-4, help='index that you want to ignore')
-    parser.add_argument('--batchsize', type=float, deault=1e-4, help='batch size')
+    parser.add_argument('--batchsize', type=float, deault=2, help='batch size')
+    parser.add_argument('--scaler', type=float, deault=20, help='scaler for cosine similarity')
+    parser.add_argument('--align_loss_scaler', type=float, deault=1, help='scaler for align loss')
--- data/dataset.py
+++ data/dataset.py
@@ -1,20 +1,141 @@
 import torch
 from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
 import os
+from PIL import Image
+import random
+import numpy as np
+from collections import defaultdict
 
 class FewshotData(Dataset):
     '''
+    나는 일단
+    1. n_way n_shots 1 3 H W 그니까 모든클래스를 shot개만큼 개지고 있는 support set이랑
+    2. n_query 3 H W 의 query set을 만들어보고자 한다. 
+    batch size를 1이상으로 한다면 아마.. 각각 1 n_way n_shots 1 3 H W, 1 n_query 3 H W 이 되겠지만 그러면 오히려 좋게 하나의 에피소드로 취급하면된다. 맞제
     fewshot style Dataset class
     support-query pairs x num_episodes
     '''
-    def __init__(self, data_path, mode, num_class) :
+    def __init__(self, data_path, mode, n_ways, n_shots, n_query, num_episode, transforms=None) :
         super().__init__()
+        self.data_path = data_path
+        self.img_path = os.path.join(data_path, 'imgs')
+        self.mask_path = os.path.join(data_path, 'masks')
+        
         self.mode = mode
+        self.n_ways = n_ways
+        self.n_shots = n_shots
+        self.n_query = n_query
+        self.num_episode = num_episode    
+        self.classes = list(range(1, n_ways+1))
+        self.cls_to_img = self._map_cls_to_img() # dict, key:cls number, value: a list of filename which include key class
         
-        self.img_path = os.path.join(data_path, mode, 'imgs')
-        self.mask_path = os.path.join(data_path, mode, 'mask')
+    
+    def __getitem__(self, index):
+        # support sets
+        # 다 뽑고 여기에 안들어가는 이미지들 중에서 query 뽑아야해요            
+        episode = {'support_imgs':[],
+                   'support_fg_masks':[],
+                   'support_bg_masks':[],
+                   'query_imgs':[],
+                   'query_labels':[]}
+        support_sets = []
+        for cls in self.classes:
+            # support_files = random.sample(self.cls_to_img[cls], self.n_shots) # cls에 해당하는 파일들중 shot개수만큼 랜덤으로 뽑음
+            support_files = [self.cls_to_img[cls].pop() for _ in range(self.n_shots)] # 다음 에피소드에서 겹치면 안되니까 pop으로 아예 제거
+            support_set += support_files
+            sup_imgs = [TF.to_tensor(Image.open(os.path.join(self.img_path, file)).convert('RGB')) for file in support_files]
+            #TODO: fg bg 구별 함수
+            sup_masks = [self._to_bg_fg_mask(Image.open(os.path.join(self.mask_path, file) for file in support_files))]
+            #TODO: small object 제외
+            episode['support_imgs'].append(sup_imgs)
+            episode['support_bg_masks'].append([i[0]for i in sup_masks])
+            episode['support_fg_masks'].append([i[1]for i in sup_masks])
+        # support 가 바뀌므로 query는 중복의 가능성이 있어도 될 것 같아사 그냥 support만 제외한 목록에서 랜덤 선택
+        query_files =  random.sample(list(set(os.listdir(self.img_path)) - set(support_sets)), self.num_query) 
+        qry_imgs = [TF.to_tensor(Image.open(os.path.join(self.img_path, file)).convert('RGB')) for file in query_files]
+        #TODO: label화 함수
+        qry_labels = [self._to_labelmap(Image.open(os.path.join(self.mask_path, file) for file in query_files))]
+        episode['query_imgs'].append(qry_imgs)
+        episode['query_labels'].append(qry_labels)
+        return episode
         
-        self.classes = range(1, num_class+1)
+    #TODO
+    def _to_labelmap(self):
+        pass
+    
+    #TODO
+    def _to_bg_fg_mask(self, cls):
+        pass
+         
+    #TODO
+    def _cls_in_mask(mask):
+        pass
+    def _map_cls_to_img(self):
+        cls_to_img = defaultdict(list)
+        filenames = os.listdir(self.img_path)
+        for file in filenames:
+            mask = Image.open(os.path.join(self.mask_path, file))
+            #TODO: extract classes from mask
+            cls_list = _cls_in_mask(mask)
+            for cls in cls_list:
+                cls_to_img[cls].append(file)
+        return cls_to_img
+        
+        
+        
+        
+    def __len__(self):
+        return self.num_episode
+    def __getitem__(self, index):
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # self.mode = mode
+        
+        # self.img_path = os.path.join(data_path, mode, 'imgs')
+        # self.mask_path = os.path.join(data_path, mode, 'mask')
+        
+        # self.imgs = os.listdir(self.img_path)
+        
+        # self.ways = range(1, n_ways+1)
+        # self.n_ways = n_ways
+        # self.n_shots = n_shots
+        # self.max_iters = max_iters
+        
+        # self.transforms = transforms
+    
+    
+    
+    
+    # def __len__(self):
+    #     len(self.imgs)
+    # def __getitem__(self, idx):
+    #     filename = self.imgs[idx]
+    #     # query
+    #     qry_img = Image.open(os.path.join(self.qry_img_path, filename)).convert('RGB')
+    #     #TODO: grayscale여부
+    #     qry_mask = Image.open(os.path.join(self.mask_path, filename))
+    #     if self.transforms != None:
+    #         qry_img = self.transforms(qry_img)
+    #     qry_img = TF.to_tensor(qry_img)
+    #     #TODO: target class, mask 내의 클래스 중에 random하게 추출
+    #     cls = random.choice(sorted(set(np.unique(qry_mask)) & self.ways))
+        
+        # support set
+        # cls에 속하는 것들 중에 랜덤하게 shot개수만큼 뽑으면 되겠죵
+        
         
         
         
